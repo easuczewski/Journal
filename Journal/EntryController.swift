82546ae77072e75175b8b770c882a5430c7f339f
Edward Suczewski <ed.suczewski@gmail.com>
2016-12-05 17:12:40 -0500

    commit
    Signed-off-by: Edward Suczewski <ed.suczewski@gmail.com>

---
+++ Journal/EntryController.swift
@@ -0,0 +1,59 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Caleb Hicks on 10/1/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class EntryController {
+    
+    private let entriesKey = "entries"
+    
+    static let sharedController = EntryController()
+    
+    var entries: [Entry]
+    
+    init() {
+        
+        self.entries = []
+        
+        self.loadFromPersistentStorage()
+    }
+    
+    func addEntry(entry: Entry) {
+        
+        entries.append(entry)
+        
+        self.saveToPersistentStorage()
+    }
+    
+    func removeEntry(entry: Entry) {
+        
+        if let entryIndex = entries.indexOf(entry) {
+            entries.removeAtIndex(entryIndex)
+        }
+        
+        self.saveToPersistentStorage()
+    }
+    
+    func loadFromPersistentStorage() {
+        
+        let entryDictionariesFromDefaults = NSUserDefaults.standardUserDefaults().objectForKey(entriesKey) as? [Dictionary<String, AnyObject>]
+
+        if let entryDictionaries = entryDictionariesFromDefaults {
+        
+            self.entries = entryDictionaries.map({Entry(dictionary: $0)!})
+        }
+    }
+    
+    func saveToPersistentStorage() {
+        
+        let entryDictionaries = self.entries.map({$0.dictionaryCopy()})
+        
+        NSUserDefaults.standardUserDefaults().setObject(entryDictionaries, forKey: entriesKey)
+    }
+    
+}
